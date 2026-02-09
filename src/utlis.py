207cae1ee382f8e42003ca85658c5b3f516ac60e
PaulStanley0211 <PaulStanleyGanganapalli@gmail.com>
2026-02-10 00:27:41 +0530

    web application

--- src/utlis.py
+++ src/utlis.py
@@ -3,7 +3,6 @@ import sys
 from xml.parsers.expat import model
 import numpy as np
 import pandas as pd
-import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV, KFold, ParameterGrid
 
@@ -86,6 +85,16 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        import dill
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
